474092ecfe5752b065143e39de4d5e3513a43f34
Maximo Calero Sanchez (GitHub) <maximo.calero@outlook.com>
2020-02-10 19:36:07 +0100

    Temporal changes to fix issues in IE11

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import HomeTwoTone from '@material-ui/icons/HomeTwoTone';
 import InfoTwoTone from '@material-ui/icons/InfoTwoTone';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
+import { MenuBar } from '../styled/MenuBar'
+import { MenuItem } from '../styled/MenuItem'
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
@@ -19,33 +21,30 @@ const Header = () => {
     const classes = styles();
     return (
         <header>
-            <Grid container
-                direction="row"
-                justify="flex-start"
-                alignItems="flex-start"
-            >
-                <Grid item>
-                    <Button
-                        variant="contained"
-                        color="default"
-                        className={classes.button}
-                        startIcon={<HomeTwoTone />}
-                    >
-                        <Link to='/'>Home</Link>
-                    </Button>
-                </Grid>
-                <Grid item>
-                    <Button
-                        variant="contained"
-                        color="default"
-                        className={classes.button}
-                        startIcon={<InfoTwoTone />}
-                    >
-                        <Link to='/about'>About</Link>
-                    </Button>                
-                </Grid> 
-            </Grid>
-               
+            <nav>
+            <MenuBar>
+                <MenuItem><Link to='/'>Home</Link></MenuItem>
+                <MenuItem><Link to='/about'>About</Link></MenuItem>
+            </MenuBar>                
+                {/* <Button
+                    key={1}
+                    variant="contained"
+                    color="default"
+                    className={classes.button}
+                    startIcon={<HomeTwoTone />}
+                >
+                    <Link to='/'>Home</Link>
+                </Button>
+                <Button
+                    key={2}
+                    variant="contained"
+                    color="default"
+                    className={classes.button}
+                    startIcon={<InfoTwoTone />}
+                >
+                    <Link to='/about'>About</Link>
+                </Button>                 */}
+            </nav>
         </header>
     );
 }
---
+++ src/components/styled/MenuBar.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const MenuBar = styled.ul `
+    list-style: none; 
+    margin: 0; 
+    padding: 0; 
+    display: flex; 
+`
